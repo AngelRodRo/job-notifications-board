04be1e8e650baa1d5eac61325c536b571dc7af3c
Angel <aleangrodriguez123@gmail.com>
2019-12-27 21:51:00 -0500

    initial commit

---
+++ src/Notification/notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const notification = (props) => {
+    return (
+        <div>
+            <p>Notification content</p>
+            <p>Notification date</p>
+        </div>
+    )
+}
+
+export default notification;
